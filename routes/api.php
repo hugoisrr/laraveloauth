b095b0fb976ff8f8cc43d232ca1c9330d24123e8
Hugo Israel Ramirez Soto <hirs@os-cillation.de>
2020-10-30 11:51:17 +0100

    courses route protected with oauth

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('clients/courses', function () {
+    return \App\Model\Course::all();
+})->middleware('client');
